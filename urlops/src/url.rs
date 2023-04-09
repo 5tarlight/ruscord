6310bb75c5b3a0167a8b11e1c27e8b373116cfc9
YEAHx4 <developerstarlight@gmail.com>
2023-04-09 12:50:02 +0900

    doc

--- urlops/src/url.rs
+++ urlops/src/url.rs
@@ -1,17 +1,27 @@
 /// Base URL of discord API
 const BASE_URL: &str = "https://discord.com/api";
 
+/// Discord api URL struct.
+/// This struct is utility for creating dynamic discord api URL.
 pub struct Url {
     /// URL literal
     url: String,
+
     /// Is URL end.
     /// If the URL is end adding sub-path will panic.
     is_end: bool,
+
+    /// Discord application id. This is not author's discord id
+    #[allow(unused)]
     user_id: &'static str,
     // token: &'static str,
 }
 
 impl Url {
+    /// Create new empty URL instance.
+    /// This will make url with empty `user_id`.
+    /// If you build URL which requires user_id with instance made by this fn,
+    /// it will words in an unintended way.
     pub fn new() -> Self {
         Url {
             url: BASE_URL.to_string(),
@@ -21,6 +31,9 @@ impl Url {
         }
     }
 
+    /// Create new empty URL instance with user id.
+    /// If you want to build some URL which requries `user_id`,
+    /// this fn is perfect method.
     pub fn from(user_id: &'static str) -> Self {
         Url {
             url: BASE_URL.to_string(),
@@ -30,10 +43,30 @@ impl Url {
         }
     }
 
+    /// Build complete URL.
+    ///
+    /// # Example
+    /// ```
+    /// use urlops::url::Url;
+    ///
+    /// let url = Url::new().as_string();
+    /// assert!(url.eq("https://discord.com/api"))
+    /// ```
     pub fn as_string(&self) -> String {
         String::from(&self.url)
     }
 
+    /// This will append new path to URL.
+    ///
+    /// # Panics
+    /// Adding new path after append query is not allowed.
+    /// Because this may make some URL such as `http://test.com?id=1/oh-no`.
+    ///
+    /// ```ignore
+    /// use urlops::url::Url;
+    ///
+    /// Url::new().add_query("id", "1").add_path("oh-no")
+    /// ```
     fn add_path(&mut self, sub: &'static str) -> &mut Self {
         if self.is_end {
             panic!("Adding path after query or something else is not allowed.")
@@ -45,6 +78,10 @@ impl Url {
         self
     }
 
+    /// This function will append new query pair to URL.
+    /// `?key=value` or `&key=value`.
+    /// Also after using this fn, calling `add_path` is prohibited.
+    #[allow(unused)]
     fn add_query(&mut self, key: &'static str, value: &'static str) -> &mut Self {
         if self.is_end {
             // This means that precedent query exists.
@@ -61,6 +98,8 @@ impl Url {
         self
     }
 
+    /// Make URL literal for certain proposes.
+    /// Mostly recommended.
     pub fn get_url(&mut self, url_type: UrlType) -> String {
         match url_type {
             UrlType::Gateway => self.add_path("gateway").as_string(),
@@ -69,6 +108,7 @@ impl Url {
     }
 }
 
+/// Pre-defined URL types.
 pub enum UrlType {
     Gateway,
     GatewayBot,
@@ -78,6 +118,7 @@ pub enum UrlType {
 mod test {
     use super::*;
 
+    #[allow(unused)]
     const USER_ID: &'static str = "1234567890";
     // const TOKEN: &'static str = "THIS_IS_MY_TOKEN";
 
