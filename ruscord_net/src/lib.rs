dd038205104168105d4854f19c3ab25efa6a7d28
YEAHx4 <developerstarlight@gmail.com>
2023-04-10 00:39:29 +0900

    ruscord_net documentation

--- ruscord_net/src/lib.rs
+++ ruscord_net/src/lib.rs
@@ -2,6 +2,10 @@ use std::{collections::HashMap, future::Future};
 
 use urlops::url::Url;
 
+/// Fetch `Url` instance.
+/// This is `async` function and will return `Future<Result<HashMap<_,_>, _>>`.
+/// You can use `unwrap_future` function in non-async (sync) function to wait for
+/// future data input.
 pub async fn fetch_url(url: &Url) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
     let res = reqwest::get(url.as_string())
         .await?
@@ -11,6 +15,10 @@ pub async fn fetch_url(url: &Url) -> Result<HashMap<String, String>, Box<dyn std
     Ok(res)
 }
 
+/// Wait for future.
+/// This method is designed to await future in non-async (sync) functions.
+/// This function will block sync function which is calling this function.
+/// Also intended to use with `fetch_url` method.
 pub fn unwrap_future(
     future: impl Future<Output = Result<HashMap<String, String>, Box<dyn std::error::Error>>>,
 ) -> Result<HashMap<String, String>, ()> {
