bc360e9a7dc35ddaefc205b4b89251a89e0eee80
YEAHx4 <developerstarlight@gmail.com>
2023-04-12 19:12:25 +0900

    simple websocket server

---
+++ local_ws/src/server.ts
@@ -0,0 +1,24 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(ws, req) {
+  ws.on("error", console.error);
+
+  console.log(`New Client connected ${req.socket.remoteAddress}`);
+  // Send Hello(10) event
+  ws.send(
+    JSON.stringify({
+      op: 10,
+      d: {
+        heartbeat_interval: 45000,
+      },
+    })
+  );
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+  });
+});
+
+console.log("WebSocket Server is listening in port 8080.");
